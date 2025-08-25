756680cd161150d8452ca5f767f5daf9f855ff3c
unknown <dpwalton@ucsc.edu>
2025-08-24 19:12:44 -0700

    started messing around with PID controls

--- motor_control_test/motor_control_test.ino
+++ motor_control_test/motor_control_test.ino
@@ -1,20 +1,37 @@
 #include <Servo.h>
 #include <PID_v1.h>
+#include <AS5X47.h>
 
+// constants for motor controllers
 #define INPUT_MIN -1000
 #define INPUT_MAX 1000
 #define OUTPUT_MIN 1000
 #define OUTPUT_MAX 2000
 
+#define STEER1_ENC_PIN 10
+
+
+
 long drive = 0;
 long drive1 = 0;
 
 double Setpoint, Input, Output;
-double kp = 2;
-double ki = 5;
-double kd = 1;
+
+double kp = 0.1;
+double ki = 0.01;
+double kd = 0.1;
+
+double steer1_angle = 0;
+
+long int loopnum = 0;
+
+bool didCompute = false;
+
 PID steer1pid(&Input, &Output, &Setpoint, kp, ki, kd, DIRECT);
 Servo steer1;
+
+// Start connection to the sensor.
+AS5X47 steer1_enc(STEER1_ENC_PIN);
 
 void setup() {
   // put your setup code here, to run once:
@@ -22,15 +39,32 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
 
+  steer1pid.SetOutputLimits(1250, 1750);
+  Input = (double) steer1_enc.readAngle();
+  Setpoint = 180;
+  steer1pid.SetMode(AUTOMATIC);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  delay(1000);
-  drive1 = -450;
-  drive = map(drive1, INPUT_MIN, INPUT_MAX, OUTPUT_MIN, OUTPUT_MAX);
-  Serial.print("drive: ");
-  Serial.println(drive);
-  steer1.write(drive);
-  
+  //drive = map(drive1, INPUT_MIN, INPUT_MAX, OUTPUT_MIN, OUTPUT_MAX);
+  loopnum++;
+
+  Input = (double) steer1_enc.readAngle();
+  steer1pid.Compute();
+  steer1.write(Output);
+  if(loopnum % 1000 == 0){
+    Serial.print("\n=================\nInput: ");
+    Serial.println(Input);
+
+    Serial.print("\nOutput: ");
+    Serial.println(Output);
+    
+    Serial.print("\Computed?: ");
+    Serial.println(didCompute);
+  }
 }
+
+// int remapPIDOutput(double num){
+//   return map((int) num, INPUT_MIN, INPUT_MAX, OUTPUT_MIN, OUTPUT_MAX);
+// }
