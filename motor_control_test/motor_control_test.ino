280de52860b4243cae25b5ef8ff62e2d394f1b34
danielwalton1a4 <danielwalton1a4@gmail.com>
2025-09-14 16:28:30 -0700

    started doing pid again

--- motor_control_test/motor_control_test.ino
+++ motor_control_test/motor_control_test.ino
@@ -1,72 +1,76 @@
-// #include <Servo.h>
-// #include <PID_v1.h>
+#include <Servo.h>
+#include <PID_v1.h>
 #include <AS5X47.h>
-// #include "HCPCA9685.h"
+#include "HCPCA9685.h"
 
 // constants for motor controllers
-// #define INPUT_MINIMUM -1000
-// #define INPUT_MINIMUM 1000
-// #define OUTPUT_MINIMUM 150  //110 and 590 come from SERVO_TRIM_MIN and max in HCPCA9685.h
-// #define OUTPUT_MAXIMUM 250
+#define INPUT_MINIMUM -180     //0 to 360 is the range of the encoder
+#define INPUT_MAXIMUM 180
+#define OUTPUT_MINIMUM 150  //after doing some testing, it looks like the rangefor the PCA9685 is 0-400.
+#define OUTPUT_MAXIMUM 250  //so 200 is dead center with 0 motor drive, below is reverse and above is forward
 
 #define s1EncPin 3  //pin # for STEER1 encoder and PWM channel
 
-// #define I2CAdd 0x40       //I2C address of servo shield
+#define I2CAdd 0x40       //I2C address of servo shield
 
-// HCPCA9685 HCPCA9685(I2CAdd);
+HCPCA9685 HCPCA9685(I2CAdd);
 
-// long drive = 0;
-// long drive1 = 0;
+long drive = 0;
+long drive1 = 0;
 
-// // double Setpoint, Input, Output;
+double Setpoint, Input, Output;
+double mappedOutput = 200;
 
-// double kp = 2;
-// double ki = 0;
-// double kd = 0.1;
+double kp = 2;
+double ki = 0;
+double kd = 0.05;
 
-// double steer1_angle = 0;
+const long interval = 100;
+unsigned long previousMillis = 0;
 
-// long int loopnum = OUTPUT_MINIMUM;
-// long int realloopnum = 0;
-// long int prevInput = 0;
-
-// const long interval = 100;
-// unsigned long previousMillis = 0;
-
-// PID steer1pid(&Input, &Output, &Setpoint, kp, ki, kd, REVERSE);
-// Servo steer1;
+PID steer1pid(&Input, &Output, &Setpoint, kp, ki, kd, REVERSE);
+Servo steer1;
 AS5X47 steer1_enc(s1EncPin);
 
 void setup() {
   // put your setup code here, to run once:
-  // steer1.attach(3);
-  Serial.begin(9600);
-  // delay(2000);
+  steer1.attach(3);
+  Serial.begin(115200);
+  delay(1000);
 
-  // steer1pid.SetOutputLimits(INPUT_MINIMUM, INPUT_MINIMUM);
-  // Input = (double) steer1_enc.readAngle();
-  // Setpoint = 180;
-  // steer1pid.SetSampleTime(5);
-  // steer1pid.SetMode(AUTOMATIC);
+  steer1pid.SetOutputLimits(INPUT_MINIMUM, INPUT_MAXIMUM);
+  Input = (double) steer1_enc.readAngle();
+  Setpoint = 180;
+  steer1pid.SetSampleTime(5);
+  steer1pid.SetMode(AUTOMATIC);
 
-  // HCPCA9685.Init(SERVO_MODE);
-  // HCPCA9685.Sleep(false);
+  HCPCA9685.Init(SERVO_MODE);
+  HCPCA9685.Sleep(false);
 
 }
 
 void loop() {
   // HCPCA9685.Servo(STEER1_PIN, (int) drive);
-  
-  float angle = steer1_enc.readAngle();
-  Serial.print("s1Enc: ");
-  Serial.println(angle);
-  delay(50);
 
-  // unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
 
-  // if (currentMillis - previousMillis >= interval) {
-  //   // save the last time you blinked the LED
-  //   previousMillis = currentMillis;
+    Input = steer1_enc.readAngle();
+    Serial.print("\n\ns1Enc: ");
+    Serial.println(Input);
+
+    steer1pid.Compute();
+    Serial.print("Output: ");
+    Serial.println(Output);
+
+    mappedOutput = map(Output, INPUT_MINIMUM, INPUT_MAXIMUM, OUTPUT_MINIMUM, OUTPUT_MAXIMUM);
+    Serial.print("Mapped Output: ");
+    Serial.println(mappedOutput);
+
+    HCPCA9685.Servo(s1EncPin, (int) mappedOutput);
+
+  }
     
 
   // }
